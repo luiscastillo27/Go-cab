25bb643699d8258283a35e0d2375775b06dce679
luis mauel <luisyosemite@gmail.com>
2018-09-21 15:57:26 -0500

    init proyect

---
+++ Go-cab/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Go-cab
+//
+//  Created by Luis Castillo on 21/09/18.
+//  Copyright © 2018 Luis Castillo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,12 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Go-cab' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Go-cab
+  pod 'SwiftyJSON'
+  pod 'Alamofire'
+
+end
